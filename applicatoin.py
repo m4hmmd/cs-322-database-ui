b5780b340b5d9928137047086c1d4745ee062d0b
Mahammad Shirinov <shrnvm@gmail.com>
2019-05-15 19:04:02 +0200

    add delete functionality

--- applicatoin.py
+++ applicatoin.py
@@ -7,19 +7,23 @@ import cx_Oracle
 
 from mainWindow import Ui_MainWindow
 
-all_table_names = ['Has_Cities', 'Countries']
 
-def get_table_names():
-    # c = conn.cursor()
-    query = 'SELECT table_name FROM user_tables'
-    c.execute(query)
-    global all_table_names
-    all_table_names = sorted([x[0] for x in c.fetchall()])
 
 class MainWindow:
     places_to_search = set()  # gonna be a set of tuples (TABLE_NAME, COLUMN_NAME)
+    prim_keys = {}
+    _translate = QtCore.QCoreApplication.translate
+    all_table_names = ['Has_Cities', 'Countries']
 
     def __init__(self):
+        dsn_tns = cx_Oracle.makedsn('cs322-db.epfl.ch', '1521', sid='ORCLCDB')
+        self.conn = cx_Oracle.connect(user=r'C##DB2019_G48', password='DB2019_G48', dsn=dsn_tns)
+        c = self.conn.cursor()
+
+        self.get_table_names()
+
+        self.get_primary_keys()
+
         self.main_win = QMainWindow()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.main_win)
@@ -31,16 +35,41 @@ class MainWindow:
         self.ui.searchButton.clicked.connect(self.search_keyword)
         self.ui.addSearchDetailsButton.clicked.connect(self.add_search_details)
         self.ui.advancedSearchButton.clicked.connect(self.advanced_search_keyword)
+        self.ui.deleteSelectTableButton.clicked.connect(self.create_delete_forms)
 
     def show(self):
         self.main_win.show()
 
+    def get_table_names(self):
+        # c = conn.cursor()
+        query = 'SELECT table_name FROM user_tables'
+        c = self.conn.cursor()
+        c.execute(query)
+        self.all_table_names = sorted([x[0] for x in c.fetchall()])
+
+    def get_primary_keys(self):
+        # get primary key information
+        for table_name in self.all_table_names:
+            query = "SELECT cols.column_name \
+                    FROM all_constraints cons, all_cons_columns cols \
+                    WHERE cols.table_name = {} \
+                    AND cons.constraint_type = 'P'\
+                    AND cons.constraint_name = cols.constraint_name\
+                    ORDER BY cols.table_name, cols.position".format("'" + table_name + "'")
+            c = self.conn.cursor()
+            c.execute(query)
+            self.prim_keys[table_name] =[ x[0] for x in c.fetchall() ]
+
+        print(self.prim_keys)
+
     def populate_table_names(self):
-        self.ui.tableComboBox.addItems(all_table_names)
-        self.ui.tableComboBox_tab3.addItems(all_table_names)
+        self.ui.tableComboBox.addItems(self.all_table_names)
+        self.ui.tableComboBox_tab3.addItems(self.all_table_names)
+        self.ui.deleteTableComboBox.addItems(self.all_table_names)
 
     def get_column_names(self, table_name):
         query = "SELECT column_name FROM all_tab_columns WHERE TABLE_NAME = '" + table_name + "'"
+        c = self.conn.cursor()
         c.execute(query)
         return [x[0] for x in c.fetchall()]
 
@@ -49,7 +78,7 @@ class MainWindow:
         query = 'select * from ' + table_name
 
         # execute sql query
-        c = conn.cursor()
+        c = self.conn.cursor()
         result = c.execute(query)
 
         # find column headers
@@ -78,7 +107,7 @@ class MainWindow:
             query += ' OR '.join([' ' + col + " LIKE '%" + keyword + "%'" for col in columns])
             print(query)
             # execute sql query
-            c = conn.cursor()
+            c = self.conn.cursor()
             result = c.execute(query)
 
             col_names = self.get_column_names(table_name)
@@ -101,7 +130,7 @@ class MainWindow:
 
     def search_keyword(self):
         keyword = self.ui.searchBox.text()
-        tables_to_search = [all_table_names[i] for i, checkBox in enumerate(self.ui.checkBoxes) if checkBox.isChecked()]
+        tables_to_search = [self.all_table_names[i] for i, checkBox in enumerate(self.ui.checkBoxes) if checkBox.isChecked()]
 
         print(keyword)
         print(tables_to_search)
@@ -114,7 +143,7 @@ class MainWindow:
             query = 'select * from Countries'  # TODO
 
         # execute sql query
-        c = conn.cursor()
+        c = self.conn.cursor()
         result = c.execute(query)
 
         column_count = 5 #TODO
@@ -133,13 +162,13 @@ class MainWindow:
 
     def populate_table_checkboxes(self):
         self.ui.checkBoxes = []
-        for i, table_name in enumerate(all_table_names):
+        for i, table_name in enumerate(self.all_table_names):
             checkBox = QCheckBox(self.ui.scrollAreaWidgetContents_3)
             checkBox.setGeometry(QtCore.QRect(10, 30 + i*20, 200, 20))
             checkBox.setObjectName("checkBox" + table_name)
 
-            _translate = QtCore.QCoreApplication.translate
-            checkBox.setText(_translate("MainWindow", table_name))
+
+            checkBox.setText(self._translate("MainWindow", table_name))
             self.ui.checkBoxes.append(checkBox)
 
     def create_insert_form(self):
@@ -158,8 +187,7 @@ class MainWindow:
             label.setGeometry(QtCore.QRect(10, 10 + i*25, 131, 20))
             label.setObjectName("insertLabel_" + str(i))
 
-            _translate = QtCore.QCoreApplication.translate
-            label.setText(_translate("MainWindow", col))
+            label.setText(self._translate("MainWindow", col))
 
             self.ui.insertLabels.append(label)
 
@@ -172,7 +200,7 @@ class MainWindow:
         self.ui.submitInsertButton = QPushButton(self.ui.scrollAreaWidgetContents1)
         self.ui.submitInsertButton.setGeometry(QtCore.QRect(20, 10 + 25 * len(col_names), 113, 32))
         self.ui.submitInsertButton.setObjectName("submitInsertButton")
-        self.ui.submitInsertButton.setText(_translate("MainWindow", "Insert"))
+        self.ui.submitInsertButton.setText(self._translate("MainWindow", "Insert"))
 
         self.ui.submitInsertButton.clicked.connect(self.insert_into_tables)
 
@@ -180,7 +208,7 @@ class MainWindow:
         self.ui.cancelInsertButton = QPushButton(self.ui.scrollAreaWidgetContents1)
         self.ui.cancelInsertButton.setGeometry(QtCore.QRect(150, 10 + 25 * len(col_names), 113, 32))
         self.ui.cancelInsertButton.setObjectName("cancelInsertButton")
-        self.ui.cancelInsertButton.setText(_translate("MainWindow", "Cancel"))
+        self.ui.cancelInsertButton.setText(self._translate("MainWindow", "Cancel"))
 
         self.ui.cancelInsertButton.clicked.connect(self.finish_insert)
 
@@ -204,9 +232,9 @@ class MainWindow:
         print(query)
 
         # insert
-        c = conn.cursor()
+        c = self.conn.cursor()
         c.execute(query)
-        conn.commit()
+        self.conn.commit()
 
         self.finish_insert()
 
@@ -220,6 +248,17 @@ class MainWindow:
         self.ui.insertLabels = []
         self.ui.insertLineEdits = []
         self.ui.scrollAreaWidgetContents1.setParent(None)
+
+    def finish_delete(self):
+        for label in self.ui.deleteLabels:
+            label.setParent(None)
+        for lineEdit in self.ui.deleteLineEdits:
+            lineEdit.setParent(None)
+        self.ui.cancelDeleteButton.setParent(None)
+        self.ui.submitDeleteButton.setParent(None)
+        self.ui.deleteLabels = []
+        self.ui.deleteLineEdits = []
+        self.ui.scrollAreaWidgetContentsDelete.setParent(None)
 
     def add_search_details(self):
         # display dialog
@@ -240,7 +279,7 @@ class MainWindow:
             dialog_win.advColumnComboBox.addItems(col_names)
             dialog_win.advColumnComboBox.setCurrentIndex(0)
             dialog_win.advColumnComboBox.setCurrentText(col_names[0])
-        dialog_win.advTableComboBox.addItems(all_table_names)
+        dialog_win.advTableComboBox.addItems(self.all_table_names)
         dialog_win.advSelectTableButton.clicked.connect(table_selected)
 
         # bind buttons to actions
@@ -268,7 +307,6 @@ class MainWindow:
         self.places_to_search.add(place)
 
     def updateSelectedColumns(self):
-        _translate = QtCore.QCoreApplication.translate
         self.dialog_win.searchPlaces.setColumnCount(2)
         self.dialog_win.searchPlaces.setRowCount(0)
         self.dialog_win.searchPlaces.setHorizontalHeaderLabels(['Table Name', 'Column Name'])
@@ -285,18 +323,74 @@ class MainWindow:
     def close_dialog(self):
         self.advanced_search_dialog.close()
 
+    def create_delete_forms(self):
+        print('create forms')
+        table_name = self.ui.deleteTableComboBox.currentText()
+        prim_keys = self.prim_keys[table_name]
+
+        self.ui.scrollAreaWidgetContentsDelete = QWidget()
+        self.ui.scrollAreaWidgetContentsDelete.setGeometry(QtCore.QRect(0, 0, 939, 469))
+        self.ui.scrollAreaWidgetContentsDelete.setObjectName("scrollAreaWidgetContentsDelete")
+
+        # add labels and forms
+        self.ui.deleteLabels = []
+        self.ui.deleteLineEdits = []
+        for i, prim_key in enumerate(prim_keys):
+            label = QLabel(self.ui.scrollAreaWidgetContentsDelete)
+            label.setGeometry(QtCore.QRect(10, 10 + i * 25, 131, 20))
+            label.setObjectName("deleteLabel_" + str(i))
+
+            label.setText(self._translate("MainWindow", prim_key))
+
+            self.ui.deleteLabels.append(label)
+
+            lineEdit = QLineEdit(self.ui.scrollAreaWidgetContentsDelete)
+            lineEdit.setGeometry(QtCore.QRect(170, 10 + i * 25, 113, 25))
+            lineEdit.setObjectName("deleteLineEdit_" + str(i))
+            self.ui.deleteLineEdits.append(lineEdit)
+
+        # add submit button
+        self.ui.submitDeleteButton = QPushButton(self.ui.scrollAreaWidgetContentsDelete)
+        self.ui.submitDeleteButton.setGeometry(QtCore.QRect(20, 10 + 25 * len(prim_keys), 113, 32))
+        self.ui.submitDeleteButton.setObjectName("submitDeleteButton")
+        self.ui.submitDeleteButton.setText(self._translate("MainWindow", "Delete"))
+
+        self.ui.submitDeleteButton.clicked.connect(self.delete_from_table)
+
+        # add cancel button
+        self.ui.cancelDeleteButton = QPushButton(self.ui.scrollAreaWidgetContentsDelete)
+        self.ui.cancelDeleteButton.setGeometry(QtCore.QRect(150, 10 + 25 * len(prim_keys), 113, 32))
+        self.ui.cancelDeleteButton.setObjectName("cancelDeleteButton")
+        self.ui.cancelDeleteButton.setText(self._translate("MainWindow", "Cancel"))
+
+        self.ui.cancelDeleteButton.clicked.connect(self.finish_delete)
+
+        # add the whole content to scroll area
+        self.ui.scrollArea_tab4.setWidget(self.ui.scrollAreaWidgetContentsDelete)
+
+    def delete_from_table(self):
+        table_name = self.ui.deleteTableComboBox.currentText()
+        prim_keys = self.prim_keys[table_name]
+        values = [lineEdit.text() for lineEdit in self.ui.deleteLineEdits]
+
+        # print('actual delete')
+        # prim_key = self.ui.primaryKeyLineEdit.text()
+
+        query = 'DELETE FROM {} WHERE '.format(table_name)
+        query += " AND ".join([prim_key + " = '" + val + "'" for prim_key, val in zip(prim_keys, values)])
+        print (query)
+
+        # delete
+        c = self.conn.cursor()
+        c.execute(query)
+        self.conn.commit()
+
+        self.finish_delete()
+
     def close_connection(self):
-        conn.close()
+        self.conn.close()
 
 if __name__ == '__main__':
-    dsn_tns = cx_Oracle.makedsn('cs322-db.epfl.ch', '1521', sid='ORCLCDB')
-    conn = cx_Oracle.connect(user=r'C##DB2019_G48', password='DB2019_G48', dsn=dsn_tns)
-    c = conn.cursor()
-
-
-    get_table_names()
-
-
     app = QApplication(sys.argv)
     main_win = MainWindow()
     main_win.show()
